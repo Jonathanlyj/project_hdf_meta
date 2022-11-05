c949466c541de7a2520e7b4ea3f065c717cc82d9
Youjia Li <yll6162@acfs.ece.northwestern.edu>
2022-11-04 21:47:46 -0500

    fix on block size setting

--- C_scripts/file_dup_b_size.c
+++ C_scripts/file_dup_b_size.c
@@ -16,10 +16,12 @@ herr_t copy_object (hid_t loc_id, const char *name, const H5O_info_t *info,
 char group_name[1];
 char* old_file;
 char* new_file;
+hid_t new_file_fapl;
+hid_t d_file_id;
 int
 main (int argc, char* argv[])
 {
-    hid_t           file_id, fapl, d_file_id, file_fapl, new_file_fapl, d_file_fapl;           
+    hid_t           file_id, file_fapl, d_file_fapl;           
     herr_t          status;
 
     clock_t start, end;
@@ -60,29 +62,36 @@ main (int argc, char* argv[])
     d_file_id = H5Fcreate (new_file, H5F_ACC_TRUNC, H5P_DEFAULT, new_file_fapl);
     printf("New file created with block size: %d\n", block_size);
     
-    status = H5Fclose(d_file_id);
+    // status = H5Fclose(d_file_id);
 
     /*
      * Begin iteration using H5Ovisit
      */
     // printf ("Objects in the file:\n");
     // start = clock();    
+
     status = H5Ovisit (file_id, H5_INDEX_NAME, H5_ITER_NATIVE, copy_object, NULL);
+    
     // end = clock();
     // time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     // printf("Metadata block size: %d\n", META_BLOCK_SIZE);
     // printf("Traverse took %f seconds to execute \n", time_used);
-    d_file_id = H5Fopen(new_file, H5F_ACC_RDWR, new_file_fapl);
+
+    // d_file_id = H5Fopen(new_file, H5F_ACC_RDWR, new_file_fapl);
     d_file_fapl = H5Fget_access_plist(d_file_id);
     H5Pget_meta_block_size(d_file_fapl, &d_block_size);
     printf("Check new file metadata block size: %d\n", d_block_size);
 
 
+
     /*
      * Close and release resources.
      */
+    status = H5Pclose (file_fapl);
+    status = H5Pclose (new_file_fapl);
     status = H5Fclose (file_id);
     status = H5Fclose(d_file_id);
+
 
 
     return 0;
@@ -105,7 +114,7 @@ herr_t copy_object (hid_t loc_id, const char *name, const H5O_info_t *info,
      * the full path name and type.
      */
     herr_t status;
-    hid_t d_file_id, d_group;
+    hid_t  d_group;
 
 
     if (name[0] != '.'){          /* Root group, do not copy '.' */
@@ -113,24 +122,24 @@ herr_t copy_object (hid_t loc_id, const char *name, const H5O_info_t *info,
             case H5O_TYPE_GROUP:
                 // printf ("%s  (Group)\n", name);
                 strcpy (group_name, name);
-                d_file_id = H5Fopen(new_file, H5F_ACC_RDWR, H5P_DEFAULT);
+                // d_file_id = H5Fopen(new_file, H5F_ACC_RDWR, new_file_fapl);
                 d_group = H5Gcreate (d_file_id, group_name, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
                 // printf("group id is %d\n", d_group);
                 status = H5Gclose (d_group);
-                status = H5Fclose(d_file_id);
+                // status = H5Fclose(d_file_id);
                 break;
 
             case H5O_TYPE_DATASET:
                 
                 // printf ("%s  (Dataset)\n", name);
                 // printf("group name is %s\n", group_name);
-                d_file_id = H5Fopen(new_file, H5F_ACC_RDWR, H5P_DEFAULT);
+                // d_file_id = H5Fopen(new_file, H5F_ACC_RDWR, new_file_fapl);
                 d_group = H5Gopen(d_file_id, group_name, H5P_DEFAULT);
                 // printf("group id is %d\n", d_group);
                 // printf("name is %s\n", name);
                 H5Ocopy(loc_id, name, d_file_id, name, H5P_DEFAULT, H5P_DEFAULT);
                 status = H5Gclose(d_group);
-                status = H5Fclose(d_file_id);
+                // status = H5Fclose(d_file_id);
 
                 break;
             case H5O_TYPE_NAMED_DATATYPE:
--- block_size_test.sh
+++ block_size_test.sh
@@ -9,12 +9,13 @@ read -a strarr <<< "$1"
 new_file=${strarr[0]}"_"$2"."${strarr[1]}
 echo "./C_scripts/file_dup_b_size $1 $new_file $2"
 ./C_scripts/file_dup_b_size "$1" "$new_file" $2
+echo "Created new h5 file: $new_file"
 
 # compile h5 file reading program file_iter_l_name.c
 h5cc C_scripts/file_iter_l_name.c -o C_scripts/file_iter_l_name
 #read new h5 file, print all objects names and track I/O using darshan
-export DARSHAN_ENABLE_NONMPI=1 #for non-MPI io
-env LD_PRELOAD=/homes/yll6162/darshan/darshan-install/lib/libdarshan.so ./C_scripts/file_iter_l_name "$new_file" $2
+
+env DARSHAN_ENABLE_NONMPI=1 LD_PRELOAD=/homes/yll6162/darshan/darshan-install/lib/libdarshan.so ./C_scripts/file_iter_l_name "$new_file" $2
 
 #find the darshan log created
 log_file=`find $DARSHAN_LOG_DIR -name "*file_iter_l_name*.darshan" -cmin -1 | sort -r | head -n 1`
@@ -28,5 +29,5 @@ read -a new_strarr <<< ${strarr[0]}
 txt_file="file_iter_l_name_"${new_strarr[-1]}"_"$2.txt
 # export DXT_ENABLE_IO_TRACE=1
 
-env DXT_ENABLE_IO_TRACE=16 darshan-parser "$log_file" > ./$txt_file
+env DXT_ENABLE_IO_TRACE=1 darshan-parser "$log_file" > ./$txt_file
 echo "Generated parsed log txt: $txt_file"
