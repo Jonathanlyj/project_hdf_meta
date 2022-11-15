4143f053a008f6e3ffe4f023914d951a5a4ff6af
Youjia Li <yll6162@acfs.ece.northwestern.edu>
2022-11-15 16:02:42 -0600

    added merged log

--- python_scripts/dxt_log_offset_parser_block.py
+++ python_scripts/dxt_log_offset_parser_block.py
@@ -16,11 +16,28 @@ def collapse(reads):
         i += 1
     return new_reads
 
+def merge_block(reads):
+    size = len(reads)
+    new_reads = []
+    i = 0
+    while i < size:
+        tot = reads[i][1]
+        start = reads[i][0]
+        while (i < size - 1) and (start + tot >= reads[i + 1][0]):
+            tot = reads[i + 1][1] + reads[i + 1][0] - start
+            i += 1
+        new_reads.append((start, tot))
+        i += 1
+    return new_reads
 
 
-# test_reads = [(0, 8), (0, 16), (16, 80), (96, 512), (680, 512)]
 
-# print(collapse(test_reads))
+
+
+
+test_reads = [(0, 8), (0, 16), (16, 80), (96, 512), (600, 512)]
+
+print(merge_block(test_reads))
 
 filename = sys.argv[1]
  
@@ -37,8 +54,12 @@ for l in lines:
         reads.append((int(strings[4]), int(strings[5])))
 reads = sorted(reads, key = lambda x: x[0])
 nonconsec_reads = collapse(reads)
+merge_reads = merge_block(reads)
 print(f"number of nonconsec_read: {len(nonconsec_reads)}")
 print(f"number of consec_read: {len(reads) - len(nonconsec_reads)}")
+
+print(f"number of merged_read: {len(merge_reads)}")
+# print(merge_reads)
 
 block_offsets = [r[0] for r in nonconsec_reads]
 # print(f"non-consecutive ranges: {min(block_offsets)} ~ {max(block_offsets)}")
@@ -80,6 +101,20 @@ with open(new_filename, "w") as f:
         f.write("{:<15}".format(i))
         f.write("{:<15}".format(nonconsec_reads[i][0]))
         f.write("{:<15}\n".format(nonconsec_reads[i][1]))
+
+
+new_filename = filename.replace(".txt", "_merged.txt")
+with open(new_filename, "w") as f:
+    f.write("{:<15}".format("Segment"))
+    f.write("{:<15}".format("Offset"))
+    f.write("{:<15}\n".format("Length"))
+    for i in range(len(merge_reads)):
+        f.write("{:<15}".format(i))
+        f.write("{:<15}".format(merge_reads[i][0]))
+        f.write("{:<15}\n".format(merge_reads[i][1]))
+
+
+
 
 
 
